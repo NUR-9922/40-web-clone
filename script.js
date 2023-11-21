161b80083a19b90a7fe3cb4ae6d00061007f93a5
NUSH-9922 <nush9922@gmail.com>
2023-11-21 18:24:12 +0530

    Done

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch and insert the navigation bar component
     fetch('component/nav-bar.html')
-        .then(response => response.text())
+        .then(response => response.text())      
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
+
+            // Add a slight delay before applying styles
+            setTimeout(() => {
+                const scriptElement = document.querySelector('#navbar-container script');
+                if (scriptElement) {
+                    eval(scriptElement.innerHTML);
+                }
+            }, 50); // Adjust the delay as needed
         })
-        .catch(error => console.error('Error fetching navigation bar:', error));
+        .catch(error => console.error('Error fetching and loading navigation bar:', error));
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('component/heroSection.html')
+        .then(response => response.text())      
+        .then(data => {
+            document.getElementById('hero-sec').innerHTML = data;
 
+            // Add a slight delay before applying styles
+            setTimeout(() => {
+                const scriptElement = document.querySelector('#navbar-container script');
+                if (scriptElement) {
+                    eval(scriptElement.innerHTML);
+                }
+            }, 50); // Adjust the delay as needed
+        })
+        .catch(error => console.error('Error fetching and loading navigation bar:', error));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('component/categotySection.html')
+        .then(response => response.text())      
+        .then(data => {
+            document.getElementById('categotySection').innerHTML = data;
+
+            // Add a slight delay before applying styles
+            setTimeout(() => {
+                const scriptElement = document.querySelector('#navbar-container script');
+                if (scriptElement) {
+                    eval(scriptElement.innerHTML);
+                }
+            }, 50); // Adjust the delay as needed
+        })
+        .catch(error => console.error('Error fetching and loading navigation bar:', error));
+});
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('component/aboutSection.html')
+        .then(response => response.text())      
+        .then(data => {
+            document.getElementById('aboutPage').innerHTML = data;
+
+            // Add a slight delay before applying styles
+            setTimeout(() => {
+                const scriptElement = document.querySelector('#navbar-container script');
+                if (scriptElement) {
+                    eval(scriptElement.innerHTML);
+                }
+            }, 50); // Adjust the delay as needed
+        })
+        .catch(error => console.error('Error fetching and loading navigation bar:', error));
+});
