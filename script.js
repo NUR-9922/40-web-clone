1f1527ae43b5ffe34f72973e19c431a3293955f1
NUSH-9922 <nush9922@gmail.com>
2023-11-22 18:13:56 +0530

    ss

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function loadSection(containerId, filePath) {
         .then(response => response.text())
         .then(data => {
             document.getElementById(containerId).innerHTML += data;
-           
+
             // Add a slight delay before applying styles
             setTimeout(() => {
                 const scriptElement = document.querySelector(`#${containerId} script`);
@@ -21,7 +21,7 @@ function loadSection(containerId, filePath) {
             }, 50); // Adjust the delay as needed
         })
         .catch(error => console.error(`Error fetching and loading section ${containerId}:`, error));
-}
+};
 
 
 
@@ -51,7 +51,7 @@ function loadPageIntoElement(elementId, filePath, extractElementId) {
             }, 50); // Adjust the delay as needed
         })
         .catch(error => console.error(`Error fetching and loading page into element ${elementId}:`, error));
-}
+};
 
 
 
@@ -70,17 +70,17 @@ function extractElementById(htmlString, elementId) {
     } else {
         return '';
     }
-}
+};
 
 
 
 
-window.onload = function(){
+window.onload = function () {
     document.querySelector(".mein").style.display = "none";
-    setTimeout(function(){
+    setTimeout(function () {
         document.querySelector(".loading-screen").style.display = "none";
         document.querySelector(".loading-screen").style.opacity = "0";
         document.querySelector(".mein").style.display = "block";
-    },3000)
-}
+    }, 3000)
+};
 
