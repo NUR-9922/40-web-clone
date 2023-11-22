7ce7612c2aece605ce2189e461d62ff7f1ed81fc
NUSH-9922 <nush9922@gmail.com>
2023-11-22 16:35:31 +0530

    add loading screen

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ function loadSection(containerId, filePath) {
         })
         .catch(error => console.error(`Error fetching and loading section ${containerId}:`, error));
 }
+
+
+
+window.onload = function(){
+    document.querySelector(".mein").style.display = "none";
+    setTimeout(function(){
+        document.querySelector(".loading-screen").style.display = "none";
+        document.querySelector(".loading-screen").style.opacity = "0";
+        document.querySelector(".mein").style.display = "block";
+    },3000)
+}
+
